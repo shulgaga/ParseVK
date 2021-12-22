41a0666e5ee46b77a8d32c76d158880b5395018b
Shulgaga <pointartem@gmail.com>
2021-12-22 20:44:56 +0300

    вью и бд

--- vkapi/parse.py
+++ vkapi/parse.py
@@ -1,5 +1,8 @@
 import requests
 import datetime
+import schedule
+import time
+from .models import Main
 
 API_KEY = '6849ded36849ded36849ded3066833a5ca668496849ded309f148be98d5788daa04f463'
 v = '5.131'
@@ -16,5 +19,33 @@ def wall_get():
                             )
     data = response.json()['response']['items']
     return data
-print(wall_get())
 
+
+def file_writer(data):
+    all_info = []
+    for post in data:
+        if post['marked_as_ads'] == 0:
+            try:
+                if post['attachments'][0]['type']:
+                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
+                    all_info.append(img_url)
+                else:
+                    img_url = 'pass'
+            except:
+                pass
+            text = post['text']
+            all_info.append(text)
+            timestamp = post['date']
+            value = datetime.datetime.fromtimestamp(timestamp)
+            value = value.strftime('%Y-%m-%d %H:%M:%S')
+            all_info.append(value)
+        else:
+            pass
+    return all_info
+
+
+
+
+
+if __name__=='__main__':
+    following()
--- vkapi/views.py
+++ vkapi/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from .parse import wall_get, file_writer
 from django.http import HttpResponse
+from .models import Main
+import schedule
+import time
 
 
 def parse_request(request):
     data = wall_get()
     html = file_writer(data)
     return HttpResponse(html)
+
+def following(request):
+    data = wall_get()
+    data.save()
+    schedule.every(30).minutes.do(following)
+    while Main.status is True:
+        schedule.run_pending()
+        time.sleep(1)
+        if Main.status is False:
+            break
+    return HttpResponse(data)
+
+
 
 
