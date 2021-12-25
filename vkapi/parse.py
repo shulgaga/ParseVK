864c206ea2634a82cf69a7b809f441a9dcbb0f3e
Malyshev Aleksandr <greeshanka@gmail.com>
2021-12-25 14:57:38 +0500

    убрал версию апи в переменные окружения. изменил названия переменных. добавил параметры в вызов функции

--- vkapi/parse.py
+++ vkapi/parse.py
@@ -1,26 +1,22 @@
 import requests
 import datetime
 
-from config import API_TOKEN, ACCESS_TOKEN
+from config import API_TOKEN, ACCESS_TOKEN, VERSION
 
-def search_wall():
-    version = '5.131'
-    q = str(input('Введите имя групп: '))
-    query = str(input('Введите нужное слово: '))
-    domain = 'autosale_rus'
+
+def search_wall(group_name, search_word):
     all_info = []
     all_screen_name_group = []
+    group_name += ' объявления'
 
     group = requests.get('https://api.vk.com/method/groups.search',
                          params={
                              'access_token': ACCESS_TOKEN,
-                             'v': version,
-                             'q': q,
-                             'country_id': 1,
+                             'v': VERSION,
+                             'q': group_name,
                              'type': 'group',
                              'count': 3,
                              'sort': 6,
-                             'market': 1
                          }).json()['response']['items']
     for i in group:
         all_screen_name_group.append(i['screen_name'])
@@ -30,36 +26,38 @@ def search_wall():
         sear = requests.get('https://api.vk.com/method/wall.search',
                             params={
                                 'access_token': API_TOKEN,
-                                'v': version,
+                                'v': VERSION,
                                 'domain': i,
                                 'count': 10,
                                 'offset': 0,
-                                'query': query,
+                                'query': search_word,
                                 'owners_only': 1
                             }
                             )
         data = sear.json()['response']['items']
 
         for post in data:
-            try:
-                if post['attachments'][0]['type']:
-                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
-                    all_info.append(img_url)
-                else:
-                    return 'pass'
-            except KeyError:
-                print('Нет фото')
+            if post['marked_as_ads'] == 0:
+                try:
+                    if post['attachments'][0]['type']:
+                        img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
+                        all_info.append(img_url)
+                    else:
+                        return 'pass'
+                except KeyError:
+                    print('Нет фото')
 
-            text = post['text']
-            all_info.append(text)
-            timestamp = post['date']
-            value = datetime.datetime.fromtimestamp(timestamp)
-            value = value.strftime('%Y-%m-%d %H:%M:%S')
-            all_info.append(value)
+                text = post['text']
+                all_info.append(text)
+                timestamp = post['date']
+                value = datetime.datetime.fromtimestamp(timestamp)
+                value = value.strftime('%Y-%m-%d %H:%M:%S')
+                all_info.append(value)
         else:
             pass
 
     return all_info
 
 
-html = search_wall()
+html = search_wall(group_name=str(input('Введите имя групп: ')),
+                   search_word=str(input('Введите нужное слово: ')))
