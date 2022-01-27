3ad1155e428425a701ee1a325b3f7c0924f79fd4
Shulgaga <pointartem@gmail.com>
2022-01-27 22:54:11 +0300

    d3

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,7 +1,7 @@
 import dj_database_url
 import django_heroku
 from pathlib import Path
-from config import SECRET_KEY
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-jq24%%8u9=0^)@w$k-xwovyp8#83s&_-d+(7zl(-yf4)e5r5ph'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
