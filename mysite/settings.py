5fd77c75ff092fce84cf8d1931f28171af52871a
Shulgaga <pointartem@gmail.com>
2022-01-28 15:07:40 +0300

    problems

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -111,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- vkapi/urls.py
+++ vkapi/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 
 
-urlpatterns = [
-    # path('create_user', views.create_user),
-    # path('update_info', views.edit_info),
-    # path('scheduler', views.sch)
-]
+urlpatterns = []
